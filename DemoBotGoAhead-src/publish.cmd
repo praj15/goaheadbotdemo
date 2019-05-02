388b08d19a8cb9a7a81a850cbf59cfa24f9afd4b
praj15 <prajith15@hotmail.com>
2019-05-02 10:56:53 +0100

    Add files via upload

---
+++ DemoBotGoAhead-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=demobotgoahead-Web-Deploy.pubxml -p:Password=kmAJFsrRGH0QvSwi35DjaQ1Rx0J9DmMf2rWuv0Eo2s9RGZEzXAMPExRGEEe3
+
